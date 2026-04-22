0d62333a7276892d48fc2b3df8c1652fca32ccd8
Rithik varma <152843420+rithikvarma2640@users.noreply.github.com>
2026-04-22 23:49:31 +0530

    Update env.ts

--- backend/src/config/env.ts
+++ backend/src/config/env.ts
@@ -7,7 +7,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 // dotenv.config({ path: path.resolve(__dirname, '../../.env') });
-dotenv.config()
+dotenv.config();
 
 const envSchema = z.object({
   NODE_ENV: z.enum(['development', 'production', 'test']).default('development'),
